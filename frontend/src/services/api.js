cb1fe0ea02e7197aefb534c01e473703e1bb844b
Jonyski <geleiaeavela@gmail.com>
2025-11-23 11:48:10 -0300

    adds edit option to products

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -10,7 +10,6 @@ const apiClient = axios.create({
 });
 
 export default {
-  // TODO: Resto das funções de acesso à API
   getFornecedores(filters = null) {
     return apiClient.get("/fornecedores");
   },
@@ -18,18 +17,15 @@ export default {
     return apiClient.get(`/fornecedores/${id}`);
   },
   createFornecedor(data) {
-    // data é um objeto JS, ex: { nome: "...", cnpj: "..." }
     return apiClient.post("/fornecedores", data);
   },
   deleteFornecedor(id) {
     return apiClient.delete(`/fornecedores/${id}`);
   },
   getProdutos(filters = null) {
-    // filters pode ser um objeto, ex: { params: { 'filter-nome': 'ilike.Cerveja' } }
     return apiClient.get("/produtos", filters);
   },
   getProdutosComerciais(filters = null) {
-    // filters pode ser um objeto, ex: { params: { 'filter-nome': 'ilike.Cerveja' } }
     return apiClient.get("/produtos/comercial", filters);
   },
   getClientes(filters = null) {
@@ -50,7 +46,6 @@ export default {
   deleteByEndpoint(endpoint) {
     return apiClient.delete(endpoint);
   },
-  // --- FUNCIONÁRIOS ---
   getFuncionarios(filters = null) {
     return apiClient.get("/funcionarios", filters);
   },
@@ -60,45 +55,30 @@ export default {
   deleteFuncionario(id) {
     return apiClient.delete(`/funcionarios/${id}`);
   },
-  // --- PRODUTOS ESTRUTURAIS ---
   getProdutosEstruturais(filters = null) {
     return apiClient.get("/produtos/estrutural", filters);
   },
-  // A criação de produto é genérica (estrutural ou comercial)
   createProduto(data) {
     return apiClient.post("/produtos", data);
   },
   createVenda(data) {
-    // data: { id_cliente, id_funcionario, tipo_pagamento, ... }
     return apiClient.post("/vendas", data);
   },
-  // O backend espera que criemos os itens associados à venda
   createItemVenda(data) {
-    // data: { id_venda, id_produto, id_lote (opcional dependendo da lógica), quantidade, valor_unitario }
     return apiClient.post("/item_venda", data);
   },
-  // Auxiliar para buscar lote disponível (Lógica FIFO necessária para o item_venda)
-  // Nota: Se o seu backend não faz isso automático, o front precisa descobrir o lote.
-  // Vou assumir por enquanto que vamos listar lotes ou pegar o primeiro disponível.
   getLotesPorProduto(idProduto) {
     return apiClient.get(`/lotes/produtos/${idProduto}`);
   },
-  // --- VENDAS & HISTÓRICO ---
   getVendas(filters = null) {
     return apiClient.get("/vendas", filters);
   },
-
-  // Para buscar os itens de uma venda específica
   getItemVenda(filters = null) {
-    // Exemplo de uso: { params: { 'filter-id_venda': 'eq.1' } }
     return apiClient.get("/item_venda", filters);
   },
-  // Necessário para descobrir o nome do produto a partir do item vendido (que só tem id_lote)
   getLote(id) {
     return apiClient.get(`/lotes/${id}`);
   },
-
-  // todo: organize
   createOferta(data) {
     return apiClient.post("/ofertas", data);
   },
@@ -109,7 +89,6 @@ export default {
     return apiClient.get("/lotes", { params: filters });
   },
   createLote(data) {
-    // data espera: { id_fornecedor, id_produto, data_fornecimento, validade, preco_unitario, quantidade_inicial, estragados }
     return apiClient.post("/lotes", data);
   },
   deleteLote(id) {
@@ -124,7 +103,6 @@ export default {
   getClienteSaldo(id) {
     return apiClient.get(`/clientes/${id}/saldo`);
   },
-  // ADICIONE ISTO:
   createCliente(data) {
     return apiClient.post("/clientes", data);
       return apiClient.delete(`/lotes/${id}`);
@@ -135,4 +113,7 @@ export default {
   getPayrollReport(params) {
     return apiClient.get('/relatorios/folha-pagamento', { params });
   },
+  updateProdutoComercial(id, data) {
+    return apiClient.put(`/produtos/comercial/${id}`, data);
+},
 };
